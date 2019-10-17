adfc15e2558f3afa89722b280e4773e3401391ee
Mlodestone <54862142+Mlodestone@users.noreply.github.com>
2019-10-17 08:57:53 -0400

    working

---
+++ Game-1.js
@@ -0,0 +1,18 @@
+var whatWhere = null;
+var whatNo = null;
+
+alert("Oh your awake, Welcome.");
+alert("You died.");
+alert("but do not Worry, I am your guide,  ill guide you through he- (coughs) um.. hear");
+whatWhere = confirm("OK = what is 'here' CANCEL = where is 'here'");
+if (whatWhere == true) {
+  alert("um well, this place is a middle ground, not good not bad.")
+  whatNo = confirm("OK =  no more questions CANCEL = where is 'here'");
+  if (whatNo == true) {
+    
+  } else {
+    alert("time and place do not exist here.")
+  }
+} else {
+  alert("time and place do not exist here.")
+}
